76b38cff794be450817ff8b80a8045836f76031a
polyhy <polyhy.gwon@gmail.com>
2016-03-12 02:24:18 +0900

    Fix get_direction function and Remove is_have7 function

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -1,26 +1,21 @@
-def is_has7(x):
-    return True if x % 10 == 7 else False if x < 10 else is_has7(x // 10)
-
-
 def get_direction(x):
-    if x <= 1:
+    if x < 7:
         return 1
-    elif (x - 1) % 7 == 0 or is_has7(x - 1):
+    elif x % 10 == 7 or x % 7 == 0:
         return get_direction(x - 1) * -1
     else:
         return get_direction(x - 1)
 
 
-def ping_pong(x):
+def pingpong(x):
     if x == 1:
         return 1
     else:
-        return ping_pong(x - 1) + get_direction(x)
+        return pingpong(x - 1) + get_direction(x - 1)
 
 
-# print (pingpong(68))
-# i = 1
-# while i <= 35:
-#     print(str(i) + " : " + str(pingpong(i)))
-#     i += 1
-# print (pingpong(998))
+print (pingpong(9))
+i = 1
+while i <= 35:
+    print(str(i) + " : " + str(pingpong(i)))
+    i += 1
