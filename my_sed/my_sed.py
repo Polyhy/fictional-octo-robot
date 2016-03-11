809b85ccdf30636f36342ea2e3b5c47f4fafc16e
polyhy <polyhy.gwon@gmail.com>
2016-03-11 23:26:41 +0900

    Update function my_sed

--- my_sed/my_sed.py
+++ my_sed/my_sed.py
@@ -1,13 +1,20 @@
-import sys
+import sys, os
+
 
 def my_sed(from_word, to_word, in_file):
-    print(from_word)
-    print(to_word)
-    print(in_file)
-
+    try:
+        res = b""
+        with open(in_file, "rb") as f:
+            lines = f.readlines()
+            for line in lines:
+                res += line.replace(from_word.encode(), to_word.encode())
+        with open(in_file, "wb") as f:
+            f.write(res)
+    except IOError:
+        print(os.path.abspath(in_file) + " is Not Found")
 
 
 if len(sys.argv) >= 4:
     my_sed(sys.argv[1], sys.argv[2], sys.argv[3])
 else:
-    print("usage: python my_find.py from_word to_word file")
+    print("usage: python my_sed.py from_word to_word file")
