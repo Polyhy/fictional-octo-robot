c873c2d33a2708ba97a8e3c00cc7919d2532d389
polyhy <polyhy.gwon@gmail.com>
2016-03-17 15:40:26 +0900

    Fix #1
    - 1의 자리 외의 다른 자리에 7이 있는 경우를 제대로 체크하지 못해
      결과값이 틀리게 나온 것 같습니다.
      해결을 위해 10진수 숫자에서 특정 숫자가 있는지 확인하는 is_has 함수를 추가했습니다.

--- pingpong/pingpong.py
+++ pingpong/pingpong.py
@@ -1,7 +1,12 @@
+def is_has(n, x):    
+    return True if x % 10 == n else False if x < 10 else is_has(n, x // 10)  
+
+
 def get_direction(x):
     if x < 7:
         return 1
-    elif x % 10 == 7 or x % 7 == 0:
+    elif is_has(7, x) or x % 7 == 0:
+        # print("%d : change direction" % (x))
         return get_direction(x - 1) * -1
     else:
         return get_direction(x - 1)
@@ -12,3 +17,6 @@ def pingpong(x):
         return 1
     else:
         return pingpong(x - 1) + get_direction(x - 1)
+
+
+print(pingpong(100))
